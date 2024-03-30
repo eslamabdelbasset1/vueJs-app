04cf9b7c8a77e49e59b90cd5b7dc5de1c3d50515
eslamabdelbasset1 <eslamabdelbasset1@gmail.com>
2024-03-31 00:47:10 +0200

    add for loop and if condition

--- script.js
+++ script.js
@@ -4,13 +4,21 @@ createApp({
     data(){
         return{
             name: "Eslam",
+            number: 0,
             age: "25",
             link: "https://google.com",
+
+            students: ["Eslam", "Ahmed", "Mohamed"],
         };
     },
     methods:{
         sayHi(){
             return "Hello World!";
         },
+        testFun(){
+            this.number++;
+        }
     },
+
+
 }).mount("#app-root");
